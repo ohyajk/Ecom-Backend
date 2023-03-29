4bc2f4c8f104cda6ff41e2b140b4f57cabde667a
ohyajk <ohyajk@gmail.com>
2023-03-29 13:09:11 +0530

    Add del again

--- controller/product.js
+++ controller/product.js
@@ -16,6 +16,12 @@ exports.addProduct = (req, res) => {
 
 exports.delProduct = ('/:id', (req, res) => {
   let delID = req.params.id
-  Product.findOneAndDelete({_id: delID})
+  Product.findOneAndDelete({ _id: delID }, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(result);
+    }
+  });
   res.send("DELETE Request Called")
 })
